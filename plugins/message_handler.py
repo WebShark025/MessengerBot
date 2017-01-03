fd142320c9158df079eb4e4fb3d351ac281e85e9
WebShark025 <arshshia@gmail.com>
2017-01-03 14:21:32 +0330

    Finished it!

--- plugins/message_handler.py
+++ plugins/message_handler.py
@@ -17,8 +17,28 @@ def message_replier(messages):
     if message.text == "/start":
       bot.send_message(userid, language[userlang]["START_MSG"])
       return
-    if "/ban" in message.text or "/unban" in message.text:
-      return
+    if message.text:
+      if "/ban" in message.text or "/unban" in message.text:
+        return
 # GP FORWARDER
 #    bot.forward_message("-" + str(SUPPORT_GP), message.chat.id, message.message_id)
+    if message.from_user.id == 98120772:
+      if message.reply_to_message:
+        if message.reply_to_message.forward_from:
+          fromid = message.reply_to_message.forward_from.id
+          if message.text:
+            bot.send_message(fromid, message.text)
+          elif message.video or message.document or message.audio or message.sticker:
+            bot.forward_message(fromid, message.chat.id, message.message_id)
+          elif message.voice:
+            fid = message.voice.file_id
+            bot.send_voice(fromid, fid)
+          elif message.photo:
+            fid = message.photo[0].file_id
+            bot.send_photo(fromid, fid)
+          else:
+            bot.send_message(message.from_user.id, "Error sending.")
+          bot.reply_to(message, "Sent!")
+      return
     bot.forward_message(SUDO_ID, message.chat.id, message.message_id)
+
