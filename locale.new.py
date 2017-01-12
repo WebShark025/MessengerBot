5440652df9f988697f92b54cedef1283f9b8fc72
WebShark025 <arshshia@gmail.com>
2017-01-12 18:34:22 +0330

    Update locale.new.py

--- locale.new.py
+++ locale.new.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 
-
 language = { "en" : {
 "TEST_MSG" : "تست",
+"NOLANG_MSG" : "Hey! Welcome to @WebShark25_Official's private messenger! \nPlease choose your default language. \n\nسلام! به بات مسنجر @WebShark25_Official خوش آمدید!"
+"START_MSG" : "Welcome! \nPlease send your message.",
+"BANNED_MSG" : ":O You got banned from the bot!",
+"UNBANNED_MSG" : "Welcome back! You are unbanned now ;)",
 }, "fa" : {
 "TEST_MSG" : "تست",
+"NOLANG_MSG" : "NOT DEFINED.",
+"START_MSG" : "خوش آمدید! \nلطفا پیام خود را بفرستید.",
+"BANNED_MSG" : "وای! شما از بات بلاک شدید!",
+"UNBANNED_MSG" : "خوش آمدید! آن بلاک شدید.",
 }
 }
+
+
